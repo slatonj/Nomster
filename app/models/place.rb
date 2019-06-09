351a9c6011c0a523510a7405bc92d1f6acacae6c
jeff <jeffslaton@msncom>
2019-06-09 06:56:19 +0000

    add more validating

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,6 @@
 class Place < ApplicationRecord
     belongs_to :user
-    validates :name, presence: true
+    validates :name, presence: true, length:{ minimum: 5, maximum: 30} 
+    validates :address, presence: true, length:{ minimum: 10, maximum: 45}
+    validates :description, presence: true, length:{ minimum: 5, maximum: 200}
 end
