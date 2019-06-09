0ddf56ffb309d7fd7e68aa55349da9402755b41b
jeff <jeffslaton@msncom>
2019-06-09 04:30:43 +0000

    only user logged in can change things

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,6 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit. :update, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
 
     def index
         @places = Place.paginate(page: params[:page ], per_page: 5)
@@ -11,13 +12,14 @@ class PlacesController < ApplicationController
     end
 
     def create
-      current_user.places.create(place_params)
-      if @place.valid?
-      redirect_to root_path
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
+     @place = current_user.places.create(place_params)
+  
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
+ end
 
     def show
       @place = Place.find(params[:id])
@@ -35,19 +37,26 @@ class PlacesController < ApplicationController
 
     def update
       @place = Place.find(params[:id])
+
       if @place.user != current_user 
         return render plain: 'Not Allowed', status: :forbidden
       end 
 
       @place.update_attributes(place_params)
-      redirect_to root_path
+      if @place.valid?
+        redirect_to root_path
+      else
+        render :edit, status: :unprocessable_entity
+      end
     end
+    
 
     def destroy
       @place = Place.find(params[:id])
-      if @place.user !=current_userreturn render plain: 'Not Allowed', status: :forbidden
+      if @place.user != current_user
+        return render plain: 'Not Allowed', status: :forbidden
       end
-
+ 
       @place.destroy
       redirect_to root_path
     end
