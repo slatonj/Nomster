e0621ca4a96acbfcc8a56ce58e8bcc6e9e1a55b2
jeff <jeffslaton@msncom>
2019-06-09 03:32:02 +0000

    lock only user loged in can make changes

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -12,7 +12,11 @@ class PlacesController < ApplicationController
 
     def create
       current_user.places.create(place_params)
+      if @place.valid?
       redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
 
     def show
@@ -43,7 +47,7 @@ class PlacesController < ApplicationController
       @place = Place.find(params[:id])
       if @place.user !=current_userreturn render plain: 'Not Allowed', status: :forbidden
       end
-      
+
       @place.destroy
       redirect_to root_path
     end
