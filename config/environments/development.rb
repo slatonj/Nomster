6ec6ee6d2d453f0ced8003c75f649db3955edcdf
jeff <jeffslaton@msncom>
2019-06-09 05:40:15 +0000

    removed email config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,4 @@
-Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'localhost:3030' }
+Rails.application.configure do  
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -60,5 +59,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,7 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: 'jeff-slaton-nomster.herokuapp.com'}
-
+  
   # Settings specified here will take precedence over those in config/application.rb.
-end
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -93,5 +92,4 @@ end
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-
-end 
+end
