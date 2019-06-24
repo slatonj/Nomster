a30eca92570d25b04c48a6401c4fe03817b7a306
jeff <jeffslaton@msncom>
2019-06-24 22:03:12 +0000

    got AWS to work right and fixed errors

--- app/models/place.rb
+++ app/models/place.rb
@@ -9,5 +9,5 @@ class Place < ApplicationRecord
 
     validates :name, presence: true, length:{ minimum: 5, maximum: 50} 
     validates :address, presence: true, length:{ minimum: 10, maximum: 45}
-    validates :description, presence: true, length:{ minimum: 5, maximum: 200}
+    validates :description, presence: true, length:{ minimum: 5, maximum: 500}
 end
