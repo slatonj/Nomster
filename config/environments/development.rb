159201041f8721d8098d2ea0f86942aedd78b3d4
jeff <jeffslaton@msncom>
2019-06-26 15:46:13 +0000

    setting up emailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
     password: ENV['GMAIL_PASSWORD']
   }
 
-
+  config.action_mailer.default_url_options = {host: 'localhost:3030'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
