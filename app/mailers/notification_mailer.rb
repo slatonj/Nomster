c255a729aa90c0b8ff811716a99b047684784354
jeff <jeffslaton@msncom>
2019-06-26 14:53:13 +0000

    testing out eamils

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@nomsterapp.com"
 
     def comment_added
-        mail(to: "jeffslaton@msn.com",
+        mail(to: "bigjay480@gmail.com",
             subject: "A comment has been added to your place") 
     end
 end
