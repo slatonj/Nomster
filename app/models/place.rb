167d402f6980260a3ddee0879cd5536e3c06f8a1
jeff <jeffslaton@msncom>
2019-07-01 12:28:01 +0000

    couple little tweak

--- app/models/place.rb
+++ app/models/place.rb
@@ -7,7 +7,7 @@ class Place < ApplicationRecord
     after_validation :geocode
 
 
-    validates :name, presence: true, length:{ minimum: 5, maximum: 50} 
-    validates :address, presence: true, length:{ minimum: 10, maximum: 45}
+    validates :name, presence: true, length:{ minimum: 5, maximum: 100} 
+    validates :address, presence: true, length:{ minimum: 10, maximum: 100}
     validates :description, presence: true, length:{ minimum: 5, maximum: 500}
 end
